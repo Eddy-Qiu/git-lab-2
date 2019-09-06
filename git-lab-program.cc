5ebd4f2b78367f8f48ce878d407f89de183917bb
Eddy Qiu <eq296919@ohio.edu>
2019-09-06 17:27:55 -0400

    added git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Eddy Qiu>
+ *        Date: <09/06/2019>
+ * Description: first git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
